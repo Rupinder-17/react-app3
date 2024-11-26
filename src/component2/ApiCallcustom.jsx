1ab197aee58a063e2f10d1d4c8186409ff9539f7
Rupinder-17 <rupinderrandhey90@gmail.com>
2024-11-26 19:41:26 +0530

    try to fetch data fron youtube api

--- src/component2/ApiCallcustom.jsx
+++ src/component2/ApiCallcustom.jsx
@@ -3,20 +3,27 @@ import React, { useEffect } from 'react'
 
 export const ApiCallcustom = () => {
   const { data, loading, error, fetchData } = useApiHook();
+  console.log("data hook", data);
+  
   // console.log(state);
-  const handleData = () => {
-    fetchData("https://api.freeapi.app/api/v1/public/youtube/videos");
-  };
+  // const handleData = () => {
+  //   fetchData("https://api.freeapi.app/api/v1/public/youtube/videos");
+  // };
   useEffect(()=>{
     fetchData( "https://api.freeapi.app/api/v1/public/youtube/videos")
   },[])
   return (
     <div>
       <div>
-        <button onClick={() => handleData()}>Fetch Data</button>
+        {/* <button onClick={() => handleData()}>Fetch Data</button> */}
         {loading && <p>Loading...</p>}
         {error && <p>Error: {error}</p>}
         {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+        {data?.map((item, index) => (
+          <li key={index}>
+            {item?.data?.data?.items?.[0]?.snippet?.channelTitle || "No Title"}
+          </li>
+        ))}
       </div>
     </div>
   );
--- src/hooks/useApiHook.js
+++ src/hooks/useApiHook.js
@@ -5,24 +5,25 @@ const apiReducer = (state, action) => {
     return { ...state, loading: true, error: null };
   }
   if (action.type == "success") {
-    return { ...state, loading: false, data: action.result, error: null };
+    console.log("action success", action.type);
+    
+    return { ...state, loading: false, data: Array.isArray(action.result.items) ? action.result.items : [], error: null };
   }
   if (action.type == "error") {
-    return { ...state, loading: false, data: null, error: action.result };
+    return { ...state, loading: false, data: [], error: action.result };
   }
   return state;
 };
 
-const initialstate = { data: null, loading: false, error: null };
+const initialstate = { data: [], loading: false, error: null };
 export const useApiHook = () => {
   const [state, dispatch] = useReducer(apiReducer, initialstate);
+  console.log("state", state);
 
-  const fetchData =  useCallback(async (apiData) => {
+  const fetchData = useCallback(async (apiData) => {
     dispatch({ type: "start" });
     try {
-      const response = await fetch(
-        apiData
-      );
+      const response = await fetch(apiData);
       const result = await response.json();
       console.log(result);
       dispatch({ type: "success", result });
@@ -30,6 +31,6 @@ export const useApiHook = () => {
       console.log(e);
       dispatch({ type: "error", result: e.message });
     }
-  },[]);
+  }, []);
   return { ...state, fetchData };
 };
