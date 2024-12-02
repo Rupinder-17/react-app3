8fa27d92741b8551bc82d48c26325d4947ab5307
Rupinder-17 <rupinderrandhey90@gmail.com>
2024-12-02 15:53:39 +0530

    apply diffrent color on drak mode

--- src/Components/ThemeCheck.jsx
+++ src/Components/ThemeCheck.jsx
@@ -1,13 +1,16 @@
-import { useTheme } from '@/hooks/useTheme'
-import React from 'react'
+import { useTheme } from "@/hooks/useTheme";
+import React from "react";
 
 export const ThemeCheck = () => {
-    const {isDarkMode, toggleTheme, colors} = useTheme()
+  const { isDarkMode, toggleTheme, colors } = useTheme();
   return (
-
     <div>
-
-        <button onClick={toggleTheme} style={{background: colors.background , color: colors.text }}>change theme</button>
+      <button
+        onClick={toggleTheme}
+        style={{ background: colors.background, color: isDarkMode ?"red" : "blue" }}
+      >
+        change theme
+      </button>
     </div>
-  )
-}
+  );
+};
